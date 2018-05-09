a61ba720589c46c66f9469ac71588207ae85c3fc
multiangle <461445957@qq.com>
2018-05-09 21:47:05 +0800

    d

--- lfm_naive.py
+++ lfm_naive.py
@@ -9,9 +9,22 @@ __author__ = 'multiangle'
 
 import numpy as np
 import math
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
-def LFM_naive(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
+def LFM_ed1(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
+    """
+    此函数参考自 https://www.cnblogs.com/tbiiann/p/6535189.html
+    :param D: 表示需要分解的评价矩阵, type = np.ndarray
+    :param k: 分解的隐变量个数
+    :param iter_times: 迭代次数
+    :param alpha: 正则系数
+    :param learn_rate: 学习速率
+    :return: 分解得到的矩阵 U, V
+    """
+
+    pass
+
+def LFM_ed2(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
     '''
     此函数实现的是最简单的 LFM 功能
     :param D: 表示需要分解的评价矩阵, type = np.ndarray
@@ -39,12 +52,90 @@ def LFM_naive(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
         ERR2_sum = np.sum(np.sum(ERR2))
         err_list.append(ERR2_sum)
     return U, V, err_list
-    
+
+def LFM_ed3(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
+    """
+    在 ed2 的基础上，加入了item 和 user的偏置， 以及整体的偏差 miu,
+    不过ed3中的 bias 和 miu 都是常数，根据评分计算得到
+    :param D: 表示需要分解的评价矩阵, type = np.ndarray
+    :param k: 分解的隐变量个数
+    :param iter_times: 迭代次数
+    :param alpha: 正则系数
+    :param learn_rate: 学习速率
+    :return: U, V, B_u, B_v, miu, err_list
+    """
+    B_u = np.mean(D, axis=1)
+    B_v = np.mean(D, axis=0)
+    m, n = np.shape(D)
+    s_bu = np.sum(B_u) * n
+    s_bv = np.sum(B_v) * m
+    miu = (np.sum(np.sum(D)) - s_bu - s_bv) / (m * n)
+
+    U = np.random.rand(m, k)
+    V = np.random.randn(k, n)
+    B_u_expand = np.matmul(np.expand_dims(B_u, 1), np.ones([1, n]))
+    B_v_expand = np.matmul(np.ones([m, 1]), np.expand_dims(B_v, 0))
+    err_list = []
+    for t in range(iter_times):
+        D_est = np.matmul(U, V) + B_u_expand + B_v_expand + miu
+        ERR = D - D_est
+        U_grad = -2 * np.matmul(ERR, V.transpose()) + 2 * alpha * U
+        V_grad = -2 * np.matmul(U.transpose(), ERR) + 2 * alpha * V
+        U = U - learn_rate * U_grad
+        V = V - learn_rate * V_grad
+
+        ERR2 = np.multiply(ERR, ERR)
+        ERR2_sum = np.sum(np.sum(ERR2))
+        err_list.append(ERR2_sum)
+    return U, V, B_u, B_v, miu, err_list
+
+def LFM_ed4(D, k, iter_times=1000, alpha=0.01, learn_rate=0.01):
+    """
+    这里假设 bias 也是一个变量，需要迭代。
+    :param D:
+    :param k:
+    :param iter_times:
+    :param alpha:
+    :param learn_rate:
+    :return:
+    """
+    m, n = np.shape(D)
+    miu = np.mean(np.mean(D))
+    err_list = []
+
+    B_u = np.random.rand(m)
+    B_v = np.random.rand(n)
+    U = np.random.rand(m, k)
+    V = np.random.rand(k, n)
+    for t in range(iter_times):
+        B_u_expand = np.matmul(np.expand_dims(B_u, 1), np.ones([1, n]))
+        B_v_expand = np.matmul(np.ones([m, 1]), np.expand_dims(B_v, 0))
+        D_est = np.matmul(U, V) + B_u_expand + B_v_expand + miu
+        ERR = D - D_est
+
+        U_grad = -2 * np.matmul(ERR, V.transpose()) + 2 * alpha * U
+        V_grad = -2 * np.matmul(U.transpose(), ERR) + 2 * alpha * V
+        B_u_grad = -2 * np.sum(ERR, axis=1) + 2 * alpha * B_u
+        B_v_grad = -2 * np.sum(ERR, axis=0) + 2 * alpha * B_v
+        # B_u_grad = -2 * np.sum(ERR, axis=1)
+        # B_v_grad = -2 * np.sum(ERR, axis=0)
+
+        U = U - learn_rate * U_grad
+        V = V - learn_rate * V_grad
+        B_u = B_u - learn_rate * B_u_grad
+        B_v = B_v - learn_rate * B_v_grad
+
+        ERR2 = np.multiply(ERR, ERR)
+        ERR2_sum = np.sum(np.sum(ERR2))
+        err_list.append(ERR2_sum)
+
+    return U, V, B_u, B_v, miu, err_list
 
 if __name__=='__main__':
     D = np.array([[5,5,0,5],[5,0,3,4],[3,4,0,3],[0,0,5,3],[5,4,4,5],[5,4,5,5]])
-    U, V, err_list = LFM_naive(D, 3, iter_times=100, learn_rate=0.01, alpha=0.01)
+    # U, V, err_list = LFM_ed3(D, 3, iter_times=100, learn_rate=0.01, alpha=0.01)
+    _, _,_, _, _, err_list = LFM_ed4(D, 3, iter_times=10000, learn_rate=0.01, alpha=0.01)
     print(err_list[-1])
-    err_log = np.log(np.array(err_list))
-    plt.plot(err_list)
-    plt.show()
+    # err_log = np.log(np.array(err_list))
+    # plt.plot(err_list)
+    # plt.show()
